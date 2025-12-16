2d451d865c9606b67eec0bd35d09f7a133ae1e58
yogithesymbian <yogirenbox33@gmail.com>
2025-12-17 00:14:23 +0800

    fix(devops): vite

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    build: {
-        outDir: 'public', // <-- specify output directory here
-        emptyOutDir: true,
-    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
