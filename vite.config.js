e5b974ec0f6d9f949d6fbac4f43a94d087ed51a7
yogithesymbian <yogirenbox33@gmail.com>
2025-12-16 15:39:33 +0800

    fix(devops): render pgsql vercel vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    build: {
+        outDir: 'public', // <-- specify output directory here
+        emptyOutDir: true,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
